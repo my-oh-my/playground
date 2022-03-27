cb2639aa669f9caba399038b7d6f5f32df94f0e6
Rafał Krasowski <my-oh-my@users.noreply.github.com>
2022-03-27 16:08:39 +0200

    Introduce Expert Advisor

---
+++ python/expert-advisor/ea/main.py
@@ -0,0 +1,6 @@
+def last_goodbye(name):
+    print(f'See You, {name}')
+
+
+if __name__ == '__main__':
+    last_goodbye('Space Cowboy')
---
+++ python/expert-advisor/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='expert-advisor',
+    version='1.0.0',
+    description='Trading Expert Advisor for XTB API',
+    author='me',
+    url='https://github.com/my-oh-my/playground/python/expert-advisor',
+    packages=['ea']
+)
